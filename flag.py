38d3c77a383c9d24ad6c8d986dcc262b5f001469
Danial Rashid <sp20-bse-046@students.cuisahiwal.edu.pk>
2023-07-14 14:42:29 +0500

    pakistani flag

---
+++ flag.py
@@ -0,0 +1,110 @@
+import turtle
+wn = turtle.Screen()
+wn.bgcolor("black")
+turtle = turtle.Turtle()
+turtle.speed(5)
+turtle.penup()
+turtle.shape("turtle")
+
+def drawFillRectangle(x, y, length, width, color):
+    turtle.goto(x,y)
+    turtle.pendown()
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.forward(width)
+    turtle.right(90)
+    turtle.forward(length)
+    turtle.right(90)
+    turtle.end_fill()
+    turtle.penup()
+
+def drawStar(x,y,color,length) :
+    turtle.goto(x,y)
+    turtle.setheading(0)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.color(color)
+    for turn in range(0,5) :
+        turtle.forward(length)
+        turtle.right(144)
+    turtle.end_fill()
+    turtle.penup()
+
+def drawCircle(x,y,color,radius) :
+    turtle.goto(x,y)
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()
+
+def drawMoon (x,y,color,radius):
+    turtle.up()
+    turtle.goto(x,y)
+    turtle.color(color)
+    turtle.begin_fill()
+    turtle.circle(radius)
+    turtle.end_fill()  
+
+def drawGreen() :
+    x = -230
+    y = 125
+    color = 'dark green'
+    drawFillRectangle(x, y, 280, 460, color)
+
+def drawWhite() :
+    x = -230
+    y =  125
+    color = 'white'
+    drawFillRectangle(x, y, 280, 130, color)
+
+def Star() :
+        x = 70
+        y = 30
+        color = 'white'
+        drawStar(x, y, color, 50)
+  
+def Circle() :
+           x = 45
+           y = -100
+           color = 'white'
+           drawCircle(x, y, color, 80)
+
+def Moon():
+            x = 65
+            y = -72
+            color = 'dark green'
+            drawMoon(x, y, color, 70)
+             
+drawGreen()
+drawWhite()
+Circle()
+Moon()
+Star()
+
+# Write text on the flag
+turtle.goto(125, -200)
+turtle.color('green')
+turtle.write("HAPPY INDEPENDENCE DAY", align="center", font=("Arial", 15, "bold"))
+turtle.goto(125, -230)
+turtle.color('white')
+turtle.write("14th AUGUST", align="center", font=("Arial", 15, "bold"))
+turtle.goto(125, -260)
+turtle.color('Yellow')
+turtle.write("DANIAL RASHID", align="center", font=("Arial", 15, "bold"))
+turtle.goto(125, -270)
+turtle.back(20)
+
+
+
+# Draw line between "HAPPY INDEPENDENCE DAY" and "DANIAL RASHID"
+turtle.penup()
+turtle.goto(-85, -232)
+turtle.pendown()
+turtle.color('white')
+turtle.forward(400)
+
+turtle.hideturtle()
